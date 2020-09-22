30fcd5410a79f2c311b0c7cb5c203f526d637346
Ernesto Pestaña <epestanamx@gmail.com>
2020-09-22 14:24:24 -0500

    Mocks y Stubs con Sinon

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -1,17 +1,46 @@
 'use strict'
 const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
 const config = {
   logging: () => {}
 }
 
+const MetricStub = {
+  belongsTo: sinon.spy()
+}
+
 let db = null
+let sandbox = null
+let AgentStub = null
 
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.createSandbox()
+
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
+
   db = await setupDatabase(config)
+})
+
+test.afterEach(() => {
+  sandbox && sinon.restore()
 })
 
 test('Agent', t => {
   t.truthy(db.Agent)
+})
+
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was execeded')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was execeded')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
 })
