e44b22ed21d5a1e95965fe78fa04ed6808a41caf
Ernesto Pestaña <epestanamx@gmail.com>
2020-09-22 13:41:40 -0500

    Advertencia sobre borrade de base de datos

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,8 +1,24 @@
 'use strict'
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
 
+const prompt = inquirer.createPromptModule()
+
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happended!')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'postgres',
@@ -20,7 +36,8 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('{Fatal error}')} ${err.message}`)
+  console.error(err.stack)
 }
 
 setup()
