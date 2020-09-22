74a177ba1959884f9e11c56b046621537b7c9bf9
Ernesto Pestaña <epestanamx@gmail.com>
2020-09-22 09:45:24 -0500

    Implemtanción de un módulo básico de base de datos

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,8 +1,20 @@
 'use strict'
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
 
 module.exports = async function (config) {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
   const Agent = {}
   const Metric = {}
+
+  await sequelize.authenticate()
 
   return {
     Agent,
