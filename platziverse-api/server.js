1f7cf120a65fa9d824ca499c6570da169ea30c7d
Ernesto Pestaña <epestanamx@gmail.com>
2020-09-23 16:21:38 -0500

    Definición de rutas

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -2,8 +2,13 @@ const chalk = require('chalk')
 const http = require('http')
 const express = require('express')
 
+const api = require('./api')
+
 const port = process.env.PORT || 3000
 const app = express()
+
+app.use('/api', api)
+
 const server = http.createServer(app)
 
 server.listen(port, () => {
