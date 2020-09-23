78a9167484fbdac4ba3a3b6c8bc2931c6293ff45
Ernesto Pestaña <epestanamx@gmail.com>
2020-09-23 18:30:37 -0500

    Conexión a la base de datos

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -3,24 +3,45 @@ const debug = require('debug')('platziverse:api')
 const chalk = require('chalk')
 const http = require('http')
 const express = require('express')
-const { handleFatalError } = require('platziverse-utils')
+const { configDb, handleFatalError } = require('platziverse-utils')
+const db = require('platziverse-db');
+
+let services
+let Agent
+let Metric
 
 const api = require('./api')
 
 const port = process.env.PORT || 3000
 const app = express()
 
+app.use('*', async (req, res, next) => {
+  if (!services) {
+    try {
+      services = await db(configDb('platziverse:api:db'))
+      Agent = services.Agent
+      Metric = services.Metric
+
+      debug('Connected to database')
+    } catch (e) {
+      return next(e)
+    }
+  }
+
+  next()
+})
+
 app.use('/api', api)
 
 app.use((err, req, res, next) => {
   debug(`Error: ${err.message}`)
-  
+
   if (err.message.match(/not found/)) {
     return res.status(404).send({ error: err.message })
   }
 })
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 if (!module.parent) {
   process.on('uncaughtException', handleFatalError)
--- platziverse-api/tests/api-tests.js
+++ platziverse-api/tests/api-tests.js
@@ -7,7 +7,7 @@ test.serial('/api/agents', async t => {
   const res = await request(server)
     .get('/api/agents')
 
-    t.regex(res.headers['content-type'], /json/, 'response Content Type should be json type')
-    t.deepEqual(res.statusCode, 200, 'response status code should be 200')
-    t.deepEqual(res.body, {}, 'response body should be the expected')
+  t.regex(res.headers['content-type'], /json/, 'response Content Type should be json type')
+  t.deepEqual(res.statusCode, 200, 'response status code should be 200')
+  t.deepEqual(res.body, {}, 'response body should be the expected')
 })
